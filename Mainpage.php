a9593aa3937446e2abbec09f97cc15c376869ff4
Poudouu <arnaud.lamblotte@gmail.com>
2016-02-02 16:33:23 +0100

    Initial use

---
+++ Mainpage.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+				<title>Shavent</title>
+		<link rel="stylesheet" href="style_mainpage.css">
+	</head>
+		<div class="outern">
+			<header class="header">
+				<nav class="header_upperline">
+					<a class="logo" href="/fr" style="background-image:url(logo.png);" ></a>
+					<a class="sign_in" href="/fr">S'identifier</a>
+				</nav>
+			</header>
+		</div>	
+	</body>
+</html>
