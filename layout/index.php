f7d831038668cec60c1c666553114728c372437e
Poudouu <arnaud.lamblotte@gmail.com>
2016-02-03 16:43:53 +0100

    Commit

---
+++ layout/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+				<title>Shavent</title>
+		<link rel="stylesheet" href="../style_mainpage.css">
+	</head>
+        <body class="body">
+		<div class="outern"  style="background-image: url(../drawable/header_bg.jpg)">
+			<header class="header">
+				<nav class="header_upperline">
+					<a class="logo" href="/fr" style="background-image:url(../drawable/logo.png);" ></a>
+                                        <div class="connect">
+                                            <a class="sign_in" href="subscribe.php">S'inscrire</a>
+                                            <a class="connection" href="/fr">S'identifier</a>
+                                        </div>
+				</nav>
+			</header>
+		</div>	
+	</body>
+</html>
---
+++ layout/subscribe.php
@@ -0,0 +1,43 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+				<title>Shavent</title>
+		<link rel="stylesheet" href="../style_subscribe.css">
+	</head>
+        <body>
+		<div class="outern"  style="background-image: url(../drawable/header_bg.jpg)">
+			<header class="header">
+				<nav class="header_upperline">
+					<a class="logo" href="/fr" style="background-image:url(../drawable/logo.png);" ></a>
+				</nav>
+			</header>
+		</div>	
+                <div class="body_subscribe">
+    
+                    <?php
+                    /*
+                    try{
+                    $bdd = new PDO('mysql:host=localhost;dbname=users;charset=utf8','root','');
+                    } 
+                    catch (Exception $e){
+                        die('Erreur :' .$e->getMessage());
+                    }
+                    $response = $bdd->query('SELECT * FROM subscription');
+                    $donnees=$response->fetch();
+                    ?>
+                        ID: <?php echo $donnees['id'];?> <br/>
+                        Username: <?php echo $donnees['login']?> <br/>
+                        password: <?php echo $donnees['password']?> <br/>
+                        email : <?php echo $donnees['email']?> <br/>
+                        Date: <?php echo $donnees['date_inscription']?> <br/>
+                    <?php
+                   
+                    $response->closeCursor();
+                    
+                    */ 
+                    ?>
+
+                </div>
+            
+	</body>
+</html>
